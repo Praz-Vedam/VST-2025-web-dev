f5227c376de558a4e30e520789c84b66c7ebf786
Praz-Vedam <prasanna.swain@vedam.org>
2026-02-17 11:13:37 +0530

    b1 arrays filter

---
+++ semester-2/batch-1/lec-13/react-2/src/App.jsx
@@ -0,0 +1,36 @@
+import "./App.css";
+import ArrayMethods from "./components/ArrayMethods";
+import Home from "./components/Home";
+import Lists from "./components/Lists";
+import "./index.css";
+
+// function Counter(props){
+
+// return <h1>{props.initialValue}</h1>
+
+// }
+
+// function MultiCounter(){
+
+// return <>
+// <Counter initialValue={10}/>
+// <Counter initialValue={0}/>
+// <Counter initialValue={25}/>
+// <Counter initialValue={"im string"}/>
+// </>
+
+// }
+
+function App() {
+  return (
+    <>
+      {/* code here */}
+      <Lists />
+      {/* <ArrayMethods /> */}
+      {/* <Home/> */}
+      {/* <MultiCounter/> */}
+    </>
+  );
+}
+
+export default App;
