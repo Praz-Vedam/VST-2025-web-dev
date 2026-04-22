a60edfaef0fb368fcd1923d4a68c04126fd0faea
Praz-Vedam <prasanna.swain@vedam.org>
2026-04-22 11:00:35 +0530

    firestore

--- semester-2/Whatsapp-Clone/src/App.jsx
+++ semester-2/Whatsapp-Clone/src/App.jsx
@@ -1,26 +1,41 @@
-import Home from "./Components/Home"
-import {Routes , Route} from 'react-router-dom'
-import Login from "./Components/Login"
-import Chat from "./Components/Chat"
-import PageNotFound from "./Components/PageNotFound"
+import Home from "./Components/Home";
+import { Routes, Route } from "react-router-dom";
+import Login from "./Components/Login";
+import Chat from "./Components/Chat";
+import ProtectedRoute from "./POC/Routing/ProtectedRoute"
+import { useState } from "react";
 
 function App() {
+    const [isLoggedIn, setisLoggedIn] = useState(true);
   
-
   return (
     <>
-     {/* <h1>WhatsApp Clone</h1> */}
-      <Routes>
-        <Route path="/" element={<Home></Home>}  > </Route>
-        <Route path="/login" element={<Login />}  > </Route>
-        <Route path="/chat/:uniqueId" element = {<Chat></Chat>}></Route>
-        <Route path="*" element={<PageNotFound/>}></Route>
+      {/* <h1>WhatsApp Clone</h1> */}
+     <Routes>
+        <Route
+          path="/"
+          element={
+            <ProtectedRoute isLoggedIn={isLoggedIn}>
+              <Home setisLoggedIn={setisLoggedIn}></Home>
+            </ProtectedRoute>
+          }
+        >
+          <Route
+            path="/chat/:uniqueId"
+            element={
+              <ProtectedRoute isLoggedIn={isLoggedIn}>
+                <Chat setisLoggedIn={setisLoggedIn}></Chat>
+              </ProtectedRoute>
+            }
+          ></Route>
+        </Route>
+        <Route
+          path="/login"
+          element={<Login setisLoggedIn={setisLoggedIn} />}
+        ></Route>
       </Routes>
-  
     </>
-  )
+  );
 }
 
-export default App
-
-
+export default App;
--- semester-2/Whatsapp-Clone/src/components/Login.jsx
+++ semester-2/Whatsapp-Clone/src/components/Login.jsx
@@ -1,8 +1,48 @@
 import React from "react";
-import { Fingerprint ,LogIn} from "lucide-react";
+import { Fingerprint, LogIn } from "lucide-react";
+import { useNavigate } from "react-router-dom";
+import { signInWithPopup } from "firebase/auth";
+import { GoogleAuthProvider } from "firebase/auth";
+import { auth,db } from "../../firebase";
+import {doc , setDoc}  from 'firebase/firestore'
 
 
-function Login() {
+
+function Login(props) {
+  const setisLoggedIn = props.setisLoggedIn;
+  const navig = useNavigate();
+  console.log(navig);
+
+async function createUser(userInfo) {
+const userObj = userInfo.user;
+
+// const displayName =  userObj.displayName
+// const email =  userObj.email
+const {displayName,email,photoURL,uid} = userObj;
+const timeStamp = Date.now();
+
+//writing the doc to firebase
+    await setDoc(doc(db , "users" ,uid), {
+      email : email,
+      name: displayName,
+      profile_pic : photoURL,
+      last_seen: timeStamp,
+      status:"I'm busy"
+    })
+
+  }
+
+  const handleLogin = async () => {
+    const userData = await signInWithPopup(auth, new GoogleAuthProvider());
+    console.log("userData", userData);
+
+    //write the user data to firestore data
+    await createUser(userData);
+
+    setisLoggedIn(true);
+    alert("logged in");
+    navig("/");
+  };
   return (
     <>
       <div className="h-[220px] bg-[#04a784]">
@@ -14,14 +54,20 @@ function Login() {
 
       <div className="bg-[#eff2f5] h-[calc(100vh-220px)] flex justify-center relative">
         <div className="bg-white h-[80%] w-[60%] flex flex-col items-center justify-center absolute top-[-100px] gap-4">
-          {/* fingerprint */} 
-        <Fingerprint className="h-20 w-20 text-[#04a784]"  strokeWidth={1.5}></Fingerprint>
+          {/* fingerprint */}
+          <Fingerprint
+            className="h-20 w-20 text-[#04a784]"
+            strokeWidth={1.5}
+          ></Fingerprint>
           <div>Sign In</div>
           <div>Sign in with your google account to get started</div>
-          <button className="bg-[#04a784] flex gap-2 p-4 rounded-xl">
+          <button
+            className="bg-[#04a784] flex gap-2 p-4 rounded-xl"
+            onClick={handleLogin}
+          >
             <div> Sign in with google</div>
             <LogIn></LogIn>
-            </button>
+          </button>
         </div>
       </div>
     </>
